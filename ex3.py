52d55f74528a899841a29029af51f0865890744f
Lakshmi Narayanan Narasimhan <grabyourfreedom@gmail.com>
2019-01-30 16:43:39 +0530

    Initial Version

---
+++ ex3.py
@@ -0,0 +1,29 @@
+# Print a label that we are counting chickens
+print("I will now count  my chickens:")
+
+# Count the chickens
+print("Hens", 25.0 + 30.0 / 6.0)
+# count the Roosters
+print("Roosters", 100.0 - 25.0 * 3 % 4)
+
+# Count the eggs
+print("Now, I will count the eggs:")
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
+
+# Check whether 5 is lesser than 5 - 7s
+print("Is it true that 3 + 2 < 5 - 7?")
+print(3.0 + 2.0 < 5.0 - 7.0)
+
+# Let us do some addition and subtraction
+print("What is 3 + 2?", 3.0 + 2.0)
+print("What is 5 - 7?", 5.0 - 7.0)
+
+# Let us converse
+print("Oh, that's why it's False")
+# Some  more conversation
+print("How about some more.")
+
+# Some decisionss
+print("Is it greater?", 5.0 > -2.0)
+print("Is it greater or equal?", 5.0 >= -2.0)
+print("is it less or equall?", 5.0 <= -2.0)
